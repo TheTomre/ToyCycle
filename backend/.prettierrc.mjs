ff38aa66e9e1c727fe8fb46f1de81660923071d7
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-05-20 14:28:32 +0300

    Added eslint, prettier, husky

---
+++ backend/.prettierrc.mjs
@@ -0,0 +1,11 @@
+/**
+ * @type {import("prettier").Options}
+ */
+const config = {
+  arrowParens: "avoid",
+  endOfLine: "lf",
+  quoteProps: "preserve",
+  trailingComma: "none"
+};
+
+export default config;
