23af687264a5a88312d1b056067fd9c7861890e4
TheTomre <120444434+TheTomre@users.noreply.github.com>
2024-06-06 04:02:29 +0300

    create toySlice and update store

--- frontend/src/features/toy/toySlice.tsx
+++ frontend/src/features/toy/toySlice.tsx
@@ -1,24 +1,55 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
-import { ToyState } from "./toyTypes";
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { Toy } from "./toyTypes";
 
-const initialState: ToyState = {
-  toys: []
+export interface ToysState {
+  toys: Toy[];
+  status: "idle" | "loading" | "succeeded" | "failed";
+  error: string | null;
+}
+
+const initialState: ToysState = {
+  toys: [],
+  status: "idle",
+  error: null
 };
 
-const toySlice = createSlice({
-  name: "toy",
-  initialState,
-  reducers: {
-    storeToyState(state, action: PayloadAction<ToyState>) {
-      const { toys } = action.payload;
-      state.toys = toys;
-    },
-    cleaeToyState(state) {
-      state.toys = [];
+export const fetchToys = createAsyncThunk<Toy[], void, { rejectValue: string }>(
+  "toys/fetchToys",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await fetch("http://localhost:3001/api/v1/toys");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      return data.data as Toy[]; // Предполагая, что данные находятся в поле `data`
+    } catch (err) {
+      return rejectWithValue("Failed to fetch toys");
     }
+  }
+);
+
+const toySlice = createSlice({
+  name: "toys",
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(fetchToys.pending, state => {
+        state.status = "loading";
+      })
+      .addCase(fetchToys.fulfilled, (state, action: PayloadAction<Toy[]>) => {
+        state.status = "succeeded";
+        state.toys = action.payload;
+      })
+      .addCase(
+        fetchToys.rejected,
+        (state, action: PayloadAction<string | undefined>) => {
+          state.status = "failed";
+          state.error = action.payload || "Failed to fetch toys";
+        }
+      );
   }
 });
 
-export const { cleaeToyState, storeToyState } = toySlice.actions;
 export default toySlice.reducer;
