0cb712d237f6a57f9752d26dcbfae673a01a115d
Valeriia Melnikov <valeriia.melnikov@gmail.com>
2024-06-19 23:16:25 +0300

    fix: correct link to profile

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -18,21 +18,23 @@ function Login() {
   return (
     <span className="flex gap-6 sm:gap-4">
       {isAuthenticated ? (
-        <Link to="/profile" className="flex gap-2 sm:gap-4">
-          <Avatar className="w-12 h-12 hover:scale-105 transition-all duration-300">
-            <AvatarImage src={user?.picture || "./assets/icons/avatar.svg"} />
+        <>
+          <Link to="/profile" className="flex gap-2 sm:gap-4">
+            <Avatar className="w-12 h-12 hover:scale-105 transition-all duration-300">
+              <AvatarImage src={user?.picture || "./assets/icons/avatar.svg"} />
 
-            <AvatarFallback>
-              <span className="block p-0 bg-cover w-12 h-12  bg-norepeat bg-[url('./assets/icons/avatar.svg')]" />
-            </AvatarFallback>
-          </Avatar>
+              <AvatarFallback>
+                <span className="block p-0 bg-cover w-12 h-12  bg-norepeat bg-[url('./assets/icons/avatar.svg')]" />
+              </AvatarFallback>
+            </Avatar>
+          </Link>
           <Button
             className="hover:bg-[#3a0e7b] hover:text-white w-12 h-12 border-2 border-[#3a0e7b] p-0"
             onClick={handleLogout}
           >
             <GrLogout size={20} />
           </Button>
-        </Link>
+        </>
       ) : (
         <>
           <Avatar className="w-12 h-12 ">
